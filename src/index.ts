8b699ebfb6c6d16cbf1b50c9b3edb0e3f534bd24
Dani Akash <DaniAkash@users.noreply.github.com>
2020-01-23 07:37:02 +0000

    start server in port 8080

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { abc } from "https://deno.land/x/abc/mod.ts";
 
 const app = abc();
 
-app.get("/", c => {
-  return "Welcome to Deno Starter!";
-});
+app
+  .get("/", c => {
+    return "Welcome to Deno Starter!";
+  })
+  .start({ port: 8080 });
